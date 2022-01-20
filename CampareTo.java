53781295967150192bd5eb699f3ae75309b521fa
Anmol-Pokhriyal <84918092+Anmol-Pokhriyal@users.noreply.github.com>
2022-01-20 09:46:33 +0530

    Add files via upload

---
+++ CampareTo.java
@@ -0,0 +1,21 @@
+
+public class CampareTo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1 = "Java Example";
+		String str2 = new String("Java Example");
+		String str3 = "DIT";
+		
+//		A - 65 to Z -90
+//		a -97 to  z -122		
+		System.out.println(str1.compareTo(str2));
+		System.out.println(str1.compareTo(str3));
+
+		
+
+		
+	}
+
+}
