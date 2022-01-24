4923d412c2e80b0f7bc51c2c7f865b6073e008a2
Anmol-Pokhriyal <84918092+Anmol-Pokhriyal@users.noreply.github.com>
2022-01-24 10:32:31 +0530

    Add files via upload
    Simple program to find factorial of a number.

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+public class Factorial {
+
+	public static void main(String[] args) {
+
+//		5! = 5 * 4 * 3 * 2 * 1 = 120
+
+		int i, fact = 1, n;
+
+		n = 5;
+
+		for(i = 1; i <= n; i++)
+		{
+			fact *= i;
+		}	
+
+		System.out.println("Factorial is: "+fact);
+	}
+
+}
