452742562e8ca304bafd3c91d99737d762410e48
Anmol-Pokhriyal <84918092+Anmol-Pokhriyal@users.noreply.github.com>
2022-05-09 23:49:53 +0530

    Add files via upload

---
+++ AutoboxingVsUnboxing.java
@@ -0,0 +1,22 @@
+
+public class AutoboxingVsUnboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+//		char ch='A';
+//		Autoboxing
+//		Character a=ch;
+//		System.out.println(a);
+		
+		
+//		Unboxing 
+		Character ch='f';
+		char a=ch;
+		System.out.println("a");
+		
+		
+	}
+
+}
